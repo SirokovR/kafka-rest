6e2c0ddb70522502741425258f64ccb675015026
laisk <laisk1@hotmail.com>
2020-12-03 15:20:11 +0200

    init commit

---
+++ src/main/java/com/example/kafkarest/KafkaRestApplication.java
@@ -0,0 +1,22 @@
+package com.example.kafkarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@EnableKafka
+@SpringBootApplication
+public class KafkaRestApplication {
+
+    @KafkaListener(topics="msg")
+    public void msgListener(String msg){
+        System.out.println(msg);
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaRestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/kafkarest/controller/MsgController.java
@@ -0,0 +1,20 @@
+package com.example.kafkarest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("msg")
+public class MsgController {
+
+    @Autowired
+    private KafkaTemplate<Long, String> kafkaTemplate;
+
+    @PostMapping
+    public void sendOrder(Long msgId, String msg){
+        kafkaTemplate.send("msg", msgId, msg);
+    }
+}
